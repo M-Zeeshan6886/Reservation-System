85a0db2e6a12a8ae72249874420d7eee62329391
M-Zeeshan6886 <mzxeeshuu@gmail.com>
2023-02-10 16:23:01 +0500

    SubBAr

--- src/Modules/Reservation schedule/Rschedule.jsx
+++ src/Modules/Reservation schedule/Rschedule.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import SideBar from "../../Common components/SideBar/SideBar";
 import "./Rschudule.scss";
-import Search from '../../assets/HomeAssets/search.png';
+import Search from "../../assets/HomeAssets/search.png";
 import Bell from "../../assets/HomeAssets/topIcon.svg";
 import proficon from "../../assets/HomeAssets/proficon.png";
+import calender from "../../assets/HomeAssets/calendar.png";
+import UserAdd from "../../assets/HomeAssets/userAdd.png";
+import plus from "../../assets/HomeAssets/plus.png";
 const Rschedule = () => {
   return (
     <>
@@ -22,10 +25,30 @@ const Rschedule = () => {
                   <div className="page-container-content-right-content-row-bar-bellicon">
                     <img src={Bell} alt="bellicon" className="bimg" />
                   </div>
-                  <img src={proficon} alt="ProfileImg"  className="profileImg"/>
+                  <img src={proficon} alt="ProfileImg" className="profileImg" />
                 </div>
               </div>
-              <div className="page-container-content-right-content-row1"></div>
+              <div className="page-container-content-right-content-row1">
+                <div className="page-container-content-right-content-row1-content">
+                  <div className="page-container-content-right-content-row1-content-calander">
+                    <img src={calender} alt="calender" />
+                    <p>Mon 05/12</p>
+                  </div>
+                  <div className="page-container-content-right-content-row1-content-seats">
+                    <div className="page-container-content-right-content-row1-content-seats-counts">
+                      <h1>0</h1>
+                      <div className="page-container-content-right-content-row1-content-seats-counts-img">
+                        <span>Available Seat</span>
+                        <img src={UserAdd} alt="UserAdd" />
+                      </div>
+                    </div>
+                    <div className="page-container-content-right-content-row1-content-seats-creats">
+                      <img src={plus} alt="plus" />
+                      <p>CREATE</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <div className="page-container-content-right-content-row2"></div>
               <div className="page-container-content-right-content-row3"></div>
               <div className="page-container-content-right-content-row4"></div>
